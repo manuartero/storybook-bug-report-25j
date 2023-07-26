aa94412684c959479809fd20e7245e40d85f2ca9
Manu Artero Anguita <manutero.developer@gmail.com>
2023-07-26 13:11:33 +0200

    workaround

--- default-js/src/storyshots.test.js
+++ default-js/src/storyshots.test.js
@@ -1,20 +1,28 @@
-/* eslint-disable no-undef */
-
 import initStoryshots from '@storybook/addon-storyshots';
 import { imageSnapshot } from '@storybook/addon-storyshots-puppeteer';
 
-const getMatchOptions = ({ context: { kind, story }, url }) => {
+const getMatchOptions = () => {
   return {
     failureThreshold: 0.2,
     failureThresholdType: 'percent',
   };
 };
 
+const beforeScreenshot = () => new Promise((resolve) => setTimeout(resolve, 300));
+
+const getGotoOptions = (_ctx) => ({
+  waitUntil: 'domcontentloaded',
+});
+
 initStoryshots({
   framework: 'react',
   suite: 'Image storyshots',
   test: imageSnapshot({
+    testTimeout: 30000,
+    setupTimeout: 30000,
     storybookUrl: process.env.STORYBOOK_URL || 'http://localhost:6006',
     getMatchOptions,
+    beforeScreenshot,
+    // getGotoOptions,
   }),
 });
